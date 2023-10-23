50fb826fd47bab3b33f3a70cefbb6d28126d7d1c
Kacper Poneta <poneciak@gmail.com>
2023-10-23 21:34:52 +0200

    chatbbb git sa bledy

--- tasks/CZA_czatBBB/main.cpp
+++ tasks/CZA_czatBBB/main.cpp
@@ -7,10 +7,10 @@ typedef std::pair<ll, ll> llPair;
 template <typename T>
 using vec = std::vector<T>;
 
+// nexts[13] = index of last ocurence
 struct Node
 {
-  int depth = 0;
-  vec<int> nexts = vec<int>(26, 0);
+  std::map<char, int> nexts;
 };
 
 vec<Node> tree;
@@ -28,64 +28,106 @@ int main() {
   ll tn = n;
   char c;
 
-  tree.push_back({0});
+  tree.push_back({});
   str s = "";
+
+  // taking input to the size of prefix
   while(s.length() < k) {
     std::cin>>c;
     s += c;
     tn--;
   }
   
+  // taking the rest of input processing the prefixes
   ll p = 0;
   while (tn--) {
     std::cin>>c;
     ll cur_leaf = 0;
     for (ll i = p; i < s.length(); i++)
     {
-      ll next_leaf = tree[cur_leaf].nexts[s[i] - 97];
-      if(next_leaf == 0) {
-        tree[cur_leaf].nexts[s[i] - 97] = tree.size();
-        tree.push_back({tree[cur_leaf].depth + 1});
-        cur_leaf = tree[cur_leaf].nexts[s[i] - 97];
+      ll next_leaf = tree[cur_leaf].nexts[s[i]];
+      if(!next_leaf) {
+        tree[cur_leaf].nexts[s[i]] = tree.size();
+        tree.push_back({});
+        cur_leaf = tree[cur_leaf].nexts[s[i]];
       } else {
         cur_leaf = next_leaf;
       }
     }
-    tree[cur_leaf].nexts[c - 97]++;
+    tree[cur_leaf].nexts[c]++;
     p++;
     s += c;
   }
 
   p = 0;
+  bool found = true;
+  bool looped = false;
   str pref = s.substr(n - k, k);
   for(ll i = 0; i < b - n; i++) {
     ll cur_leaf = 0;
+    found = true;
     for(ll j = p; j < pref.length(); j++) {
-      ll next_leaf = tree[cur_leaf].nexts[pref[j] - 97];
-      if(next_leaf == 0) {
+      ll next_leaf = tree[cur_leaf].nexts[pref[j]];
+      if(!next_leaf) {
+        if(tree[cur_leaf].nexts[13]) {
+          // prefixes start looping
+          found = true;
+          break;
+        }
+        tree[cur_leaf].nexts[13] = p;
         pref += 'a';
+        found = false;
         break;
       } else {
         cur_leaf = next_leaf;
       }
     }
 
-    if(tree[cur_leaf].depth == k) {
-      ll count = 0;
-      int cur = 0;
-      for(int j = 25; j >= 0; j--) {
-        if(tree[cur_leaf].nexts[j] >= count) {
-          cur = j;
-          count = tree[cur_leaf].nexts[j];
+    if(found) {
+      if(tree[cur_leaf].nexts[13]) {
+        // prefixes start looping
+        // std::cout<<pref<<"\n";
+        looped = true;
+        ll loop_s = tree[cur_leaf].nexts[13];
+        ll point_in_generated = n - k + loop_s;
+        ll l_to_a = a - point_in_generated;
+        ll loop_lenght = pref.length() - k - loop_s;
+        ll start = (l_to_a % loop_lenght) - 1;
+        std::cout<<start<<"\n";
+        // for(int j = 0; j < loop_lenght; j++) {
+        //   std::cout<<pref[loop_s + j];
+        // }
+        // std::cout<<"\n";
+        for(int j = 0; j <= b - a; j++) {
+          std::cout<<pref[loop_s + start];
+          start++;
+          start %= loop_lenght;
         }
+
+        break;
+      } else {
+        ll count = 0;
+        char cur = 'a';
+
+        for(auto kv : tree[cur_leaf].nexts) {
+          // std::cout<<"c: "<<kv.first<<" v: "<<kv.second<<"\n";
+          if(kv.second > count) {
+            count = kv.second;
+            cur = kv.first;
+          } 
+        }
+        tree[cur_leaf].nexts[13] = p;
+        pref += cur;
       }
-      pref += (char)(cur + 97);
     }
     p++;
   }
 
-  for(ll i = a - n + k - 1; i < b - n + k; i++) {
-    std::cout<<pref[i];
+
+  if(!looped) {
+    for(ll i = a - n + k - 1; i < b - n + k; i++) {
+      std::cout<<pref[i];
+    }
   }
 
   std::cout.flush();
